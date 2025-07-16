03bf350df63a5bdf67e90c31b864288c48a13bdf
DiegoBustos16 <diegombustos16@gmail.com>
2025-07-16 17:35:50 -0300

    Initial commit

---
+++ src/main/java/com/diegobustos/design_patterns_collection/DesignPatternsCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.diegobustos.design_patterns_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegobustos/design_patterns_collection/DesignPatternsCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegobustos.design_patterns_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
